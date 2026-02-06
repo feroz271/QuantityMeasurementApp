d21b4466c38a3eff74199d7dd357f697fcb37fac
Feroz Salim Sayyed <fe40176355@wipro.com>
2026-02-06 12:32:35 +0530

    UC4 completed

--- App/src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ App/src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -1,21 +1,16 @@
 package com.apps.quantitymeasurement;
 
-/**
- * UC3: Supported length units with conversion factors to the base unit (INCH).
- * For UC3 we need FEET and INCH. (UC4 me YARD, CENTIMETER add karenge.)
- */
 public enum LengthUnit {
-    INCH(1.0),   // base
-    FEET(12.0);  // 1 ft = 12 in
 
-    private final double toInchesFactor;
-
-    LengthUnit(double toInchesFactor) {
-        this.toInchesFactor = toInchesFactor;
+     FEET(12.0),
+    INCHES(1.0),
+    YARDS(36.0),
+    CENTIMETERS(0.393701);
+    private final double conversionFactor;
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
     }
-
-    /** Convert a value in this unit to inches (base unit). */
-    public double toBaseInches(double value) {
-        return value * toInchesFactor;
+    public double toInches(double value) {
+        return value * conversionFactor;
     }
 }
--- App/src/main/java/com/apps/quantitymeasurement/QuantityLength.java
+++ App/src/main/java/com/apps/quantitymeasurement/QuantityLength.java
@@ -1,4 +1,49 @@
 package com.apps.quantitymeasurement;
 
+import java.util.Objects;
+
 public class QuantityLength {
+    public enum LengthUnit {
+        FEET(12.0),
+        INCHES(1.0),
+        YARDS(36.0),
+        CENTIMETERS(0.393701);
+        private final double conversionFactor;
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+        public double toInches(double value) {
+            return value * conversionFactor;
+        }
+    }
+
+    public static class Length {
+        private final double value;
+        private final LengthUnit unit;
+        public Length(double value, LengthUnit unit) {
+            if (unit == null) {
+                throw new IllegalArgumentException("Unit cannot be null");
+            }
+            this.value = value;
+            this.unit = unit;
+        }
+        private double toInches() {
+            return unit.toInches(value);
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Length other = (Length) obj;
+            return Double.compare(this.toInches(), other.toInches()) == 0;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(toInches());
+        }
+        @Override
+        public String toString() {
+            return value + " " + unit;
+        }
+    }
 }
