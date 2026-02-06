1dd1065887853459e4277aad2b2718aec01be6ef
Feroz Sayyed <162626833+feroz271@users.noreply.github.com>
2026-02-06 10:51:05 +0530

    Implement QuantityLength class with methods

--- App/src/main/java/com/apps/quantitymeasurement/QuantityLength.java
+++ App/src/main/java/com/apps/quantitymeasurement/QuantityLength.java
@@ -1,4 +1,35 @@
 package com.apps.quantitymeasurement;
 
+import java.util.Objects;
+
+
 public class QuantityLength {
+
+    private final double value;
+    private final LengthUnit unit;
+    public QuantityLength(double value, LengthUnit unit) {
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null");
+        }
+        this.value = value;
+        this.unit = unit;
+    }
+    private double toInches() {
+        return unit.toBaseInches(value);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        QuantityLength other = (QuantityLength) obj;
+        return Double.compare(this.toInches(), other.toInches()) == 0;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(toInches());
+    }
+    @Override
+    public String toString() {
+        return value + " " + unit;
+    }
 }
